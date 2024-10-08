6ad683cc3ed833294cbe8e15f42ea409ffd20301
Florian Hass <f91hama@gmail.com>
2024-10-08 15:45:54 +0000

    feat: Add responsive NavBar with logo and navigation links
    - Implemented a responsive `NavBar` using React and React-Bootstrap.
    - Added logo to the Navbar.Brand section.
    - Included navigation links for "Home", "Sign in", and "Sign up" with FontAwesome icons.
    - Enabled `Navbar.Toggle` for better mobile responsiveness.
    - Used Bootstrap's `Container` for proper alignment and spacing.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-right">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
